767b9daf3440ee63efddb1d12ce92585e8be35ef
prashg1 <prashg3029@gmail.com>
2023-04-12 22:25:31 +0530

    first commit

---
+++ setupRemoteMongo.sh
@@ -0,0 +1,3 @@
+# Setup file template to upload data to MongoDB Atlas
+mongoimport --uri "mongodb+srv://prashg:mig110@qkart-node-pranjal.1zuw5aa.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv://prashg:mig110@qkart-node-pranjal.1zuw5aa.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
---
+++ src/routes/v1/user.route.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const validate = require("../../middlewares/validate");
+const userValidation = require("../../validations/user.validation");
+const userController = require("../../controllers/user.controller");
+const auth = require("../../middlewares/auth");
+
+const router = express.Router();
+
+// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Implement a route definition for `/v1/users/:userId`
+router.get(
+    '/:userId',
+    // CRIO_SOLUTION_START_MODULE_AUTH
+    auth(),
+    // CRIO_SOLUTION_END_MODULE_AUTH
+    validate(userValidation.getUser),
+    userController.getUser
+  );
+  // CRIO_SOLUTION_END_MODULE_UNDERSTANDING_BASICS
+  
+  // Supports updating address only currently
+  router.put(
+    "/:userId",
+    auth(),
+    validate(userValidation.setAddress),
+    userController.setAddress
+  );
+
+module.exports = router;
